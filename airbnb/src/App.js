9bedbf5605028abf9ab23a459cdb03ceb2b55f46
hzhang02 <zhang.haoran1@northeastern.edu>
2022-11-11 10:16:19 -0800

    include Routes but still bug as matched leaf route does not have an element

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from "react";
-import {BrowserRouter as Router,Route} from 'react-router-dom';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import NavBar from "./utility/NavBar/NavBar";
 import Home from "./pages/Home/Home";
 class App extends Component{
